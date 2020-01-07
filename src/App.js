d05fab4b37b35a77f57701815c3daaaa5e74284f
shahroz.khan <shehroz.altaf92@gmail.com>
2020-01-07 15:41:53 +0500

    Nav Drawer & Animating the Drawer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
-// import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch
+} from 'react-router-dom';
+
 import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
-    return <Router>
-        <Switch>
-            <Route path="/" exact>
-                <Users/>
-            </Route>
-            <Route path="/places/new" exact>
-                <NewPlace/>
-            </Route>
-            <Redirect to="/"/>
-        </Switch>
-    </Router>
+    return (
+        <Router>
+            <MainNavigation />
+            <main>
+                <Switch>
+                    <Route path="/" exact>
+                        <Users />
+                    </Route>
+                    <Route path="/places/new" exact>
+                        <NewPlace />
+                    </Route>
+                    <Redirect to="/" />
+                </Switch>
+            </main>
+        </Router>
+    );
 };
 
 export default App;
--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -3,11 +3,17 @@ import React from 'react';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
-    const USERS = [
-        {id: 1, name: 'Max Sche', image: '', places: 3},
-        {id: 2, name: 'Shehroz', image: 'https://miro.medium.com/fit/c/256/256/0*BHzmBK43DIDH90YD.jpg', places: 5}
-    ];
-    return <UsersList items={USERS}/>
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Max Schwarz',
+      image:
+        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
