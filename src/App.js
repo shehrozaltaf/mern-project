75eb83528ba453719b4355b251d677f60995156b
shahroz.khan <shehroz.altaf92@gmail.com>
2020-01-08 17:22:13 +0500

    Update & Delete Place, Custom Hook, Forms & validation

--- src/App.js
+++ src/App.js
@@ -8,25 +8,29 @@ import {
 
 import Users from './users/pages/Users';
 import NewPlace from './places/pages/NewPlace';
-import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
     return (
         <Router>
-            <MainNavigation/>
+            <MainNavigation />
             <main>
                 <Switch>
                     <Route path="/" exact>
-                        <Users/>
-                    </Route>
-                    <Route path="/places/new" exact>
-                        <NewPlace/>
+                        <Users />
                     </Route>
                     <Route path="/:userId/places" exact>
                         <UserPlaces />
                     </Route>
-                    <Redirect to="/"/>
+                    <Route path="/places/new" exact>
+                        <NewPlace />
+                    </Route>
+                    <Route path="/places/:placeId">
+                        <UpdatePlace />
+                    </Route>
+                    <Redirect to="/" />
                 </Switch>
             </main>
         </Router>
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,50 +1,41 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 import PlaceList from '../components/PlaceList';
 
-const Dummy_Places = [
-    {
-        id: "p1",
-        title: "Empire State Building",
-        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipP6DUOn5UGrHCK2HtNrTcIS18LAbjKPC2kWEyi5=w408-h271-k-no",
-        description: "One of the most famous location",
-        address: "20 W 34th St, New York, NY 10001, United States",
-        location: {
-            lat: 40.7484405,
-            lng: -73.9878584
-        },
-        creator: "u1",
-    }, {
-        id: "p2",
-        title: "34 St-Herald Sq",
-        imageUrl: "https://lh5.googleusercontent.com/p/AF1QipP6DUOn5UGrHCK2HtNrTcIS18LAbjKPC2kWEyi5=w408-h271-k-no",
-        description: "One of the most famous location 2",
-        address: "Q226+2P New York, United States",
-        location: {
-            lat: 40.7484445,
-            lng: -73.9878531
-        },
-        creator: "u2",
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
     },
-    {
-        id: 'p3',
-        title: 'Broadway Theatre',
-        description: 'roadway theater dating to 1924 known for hosting large musical productions in its 1,761-seat space.',
-        imageUrl:
-            'https://lh5.googleusercontent.com/p/AF1QipNsN2r6daxPpEPetrAvJgacoQBD4Tc3bH-vLsZI=w427-h240-k-no',
-        address: '1681 Broadway, New York, NY 10019',
-        location: {
-            lat: 40.763024,
-            lng: -73.984522
-        },
-        creator: 'u1'
-    }
+    creator: 'u1'
+  },
+  {
+    id: 'p2',
+    title: 'Emp. State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u2'
+  }
 ];
+
 const UserPlaces = () => {
-    const userId = useParams().userId;
-    const loadedPlaces = Dummy_Places.filter(place => place.creator === userId);
-    return <PlaceList items={loadedPlaces}/>
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
